f63a59665fe1e77fbdb00b8de9ed7b7afaaeeccd
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-04 17:23:55 +0300

    update

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,5 +1,7 @@
 // app/api/users/[id]/route.ts
+import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 // PATCH /api/users/:id
@@ -8,10 +10,25 @@ export async function PATCH(
   { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+
+    if (!session || session.user.role !== "ADMIN") {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const userId = (await params).id;
     const resolvedParams = await params;
     const { id } = resolvedParams;
     const body = await req.json();
     const { role } = body;
+
+    if (session.user.id === userId) {
+      return NextResponse.json(
+        { error: "Cannot delete your own account" },
+        { status: 400 }
+      );
+    }
 
     const updatedUser = await prisma.user.update({
       where: { id },
@@ -20,6 +37,9 @@ export async function PATCH(
 
     return NextResponse.json(updatedUser);
   } catch (error) {
-    return NextResponse.json({ error: "Failed to update user role" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to update user role" },
+      { status: 500 }
+    );
   }
 }
